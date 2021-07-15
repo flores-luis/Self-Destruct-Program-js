e851ef6c0a3a4883327609b3cfd83538f1741093
flores-luis <ricardoluis6212@gmail.com>
2021-07-14 20:14:07 -0700

    Adding directions

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Display an alert dialog with the content: Warning! This message will self-destruct in"
+
+// Display a "3.. 3.. 1.." countdown using 3 alert dialog boxes
+
+// This statement select the <h1> element and replaces its text with "BOOM!"
+
+// Log "Message destroyed!" to the console
