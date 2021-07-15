a094a881f7b2bc78c1be0d60a05c3d3476539d2c
flores-luis <ricardoluis6212@gmail.com>
2021-07-14 21:20:17 -0700

    Created a ticking alert/bomb javascript program

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 // Display an alert dialog with the content: Warning! This message will self-destruct in"
+alert("Warning! This message will self-destruct in");
 
 // Display a "3.. 3.. 1.." countdown using 3 alert dialog boxes
+alert("3..");
+alert("2..");
+alert("1..");
 
 // This statement select the <h1> element and replaces its text with "BOOM!"
+document.querySelector("h1").textContent = "🔥🔥🔥BOOM🔥🔥🔥";
 
 // Log "Message destroyed!" to the console
+console.log("Message destroyed!");
